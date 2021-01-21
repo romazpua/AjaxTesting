4f45f27e76900b3f2073feab9565b68f0a4e0d05
romazpua <romazpua@github.com>
2021-01-21 18:32:35 +0200

    create app ToDo

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 $(document).ready(function () {
 
-    const resultBlock = document.querySelector('.content')
-    const clickMeButton = document.querySelector('#click-me')
-    const getTasksButton = document.querySelector('#get-tasks')
-    const choiceSource = document.getElementById('select_content')
+    // receive tasks from the server on page load
+    getTasks().then(onTasksReceived)
 
+    //Receiving images from the server
     clickMeButton.addEventListener('click', () => {
-
         let pageNumber;
         let countPictures;
-
         if (choiceSource.value === "https://jsonplaceholder.typicode.com/photos") {
             pageNumber = '';
             countPictures = '';
@@ -17,39 +14,62 @@ $(document).ready(function () {
             pageNumber = '?page=' + document.getElementById('page-number').value
             countPictures = '&count=' + document.getElementById('countPictures').value
         }
-
         const promise = getImages(choiceSource.value, pageNumber, countPictures);
         promise
             .then(onImagesReceived)
     })
 
+    // Determine which element was clicked
+    tasksList.addEventListener('click', (e) => {
+        let target = e.target
+        if (target.tagName !== 'LI') return
+        hightLight(target)
+    })
+
+    //highlight the selected task
+    const hightLight = (el) => {
+        if (el.classList.contains('hightlight')) {
+            el.classList.remove('hightlight')
+        } else {
+            el.classList.add('hightlight')
+        }
+    }
+
+    // Button for receiving tasks
     getTasksButton.addEventListener('click', () => {
         const promise = getTasks()
         promise.then(onTasksReceived)
     })
 
-    createTask('bla1-bla1-bla1')
-        .then((data) => {
-            console.log(data)
-        })
+    // Send a task to the server
+    createTasksButton.addEventListener('click', () => {
+        createTask(taskTitle.value, taskDescription.value)
+            .then(response => console.log(response))
+        taskTitle.value = ''
+        taskDescription.value = ''
+    })
 
-    createTask('bla2-bla2-bla2')
-        .then((data) => {
-            console.log(data)
-        })
+    // Delete marked tasks
+    deleteTasksButton.addEventListener('click', () => {
+        deleteTask("a62e6f16-3175-41f1-a8c7-f9769f66fee5")
+    })
 
-
+    // Rendering tasks when they are received from the server
     function onTasksReceived(tasks) {
+        const result = document.getElementById('tasks__content')
+        result.innerHTML = ''
         tasks.forEach(
             el => {
                 const li = document.createElement('li')
-                li.innerHTML = el.title;
-                document.getElementById('tasks__content').appendChild(li)
+                li.innerHTML = el.title + ': ' + el.description;
+                li.dataset.id = el.id;
+                li.className = 'task'
+                result.appendChild(li)
             }
         )
     }
 
-
+    // Rendering images when they are received from the server
     function onImagesReceived(data) {
         data.forEach(
             el => {
@@ -60,75 +80,3 @@ $(document).ready(function () {
         )
     }
 });
-
-
-// PROMISES
-
-// console.log('Request data...')
-
-// setTimeout(()=>{
-//     console.log('Preparing data...')
-//
-//     const backendData = {
-//         server: 'aws',
-//         port: 2000,
-//         status: 'working',
-//     }
-//
-//     setTimeout(()=>{
-//         backendData.modified = true;
-//         console.log('Data received', backendData)
-//     }, 2000)
-//
-// }, 2000)
-
-
-// const p = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         console.log('Preparing data...')
-//         const backendData = {
-//             server: 'aws',
-//             port: 2000,
-//             status: 'working',
-//         }
-//         resolve(backendData)
-//     }, 2000)
-// })
-//
-// p.then(data => {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             data.modified = true
-//             resolve(data)
-//         }, 2000)
-//     })
-// })
-//     .then(clientData => {
-//         clientData.fromPromise = true
-//         return clientData
-//     })
-//     .then(dat => {
-//         console.log('Modified', dat)
-//     })
-//     .catch(err => {
-//         console.log('Error:', err)
-//     })
-//     .finally(() => {
-//         console.log('Finally')
-//     })
-
-
-// const sleep = ms => {
-//     return new Promise(resolve => {
-//         setTimeout(() => resolve(), ms)
-//     })
-// }
-
-// sleep(3000).then(() => console.log('After 3 seconds'))
-// sleep(5000).then(() => console.log('After 5 seconds'))
-
-// Promise.all([sleep(2000), sleep(5000)])
-//     .then(() => console.log('All promises'))
-//
-// Promise.race([sleep(2000), sleep(5000)])
-//     .then(() => console.log('Race promises'))
--- src/service.js
+++ src/service.js
@@ -1,26 +1,45 @@
-// function getImagesOld(choiceSource, pageNumber, countPictures) {
-//
-//     const promise = $.ajax(`${choiceSource}${pageNumber}${countPictures}`);
-//     return promise;
-// }
-
+// The function receives images from the server
 function getImages(choiceSource, pageNumber, countPictures) {
-
     const promise = axios.get(`${choiceSource}${pageNumber}${countPictures}`);
-    return promise.then((data)=>{
+    return promise.then((data) => {
         return data.data;
     });
 }
 
+// The function receives tasks from the server
 function getTasks() {
-    const promise = axios.get('https://repetitora.net/api/JS/Tasks?widgetId=15984')
+    const promise = axios.get('https://repetitora.net/api/JS/Tasks?widgetId=1336&count=30')
     return promise.then((response) => {
         return response.data;
     })
 }
 
-function createTask(title) {
-    const promise = axios.post(`https://repetitora.net/api/JS/Tasks?widgetId=15984&title=${title}`)
+// The function sends a task to the server
+function createTask(title, taskDescription) {
+    const promise = axios.post('https://repetitora.net/api/JS/Tasks', {
+        widgetId: 1336,
+        title: title,
+        description: taskDescription,
+    })
+    return promise.then((response) => {
+        return response.data;
+    })
+}
+
+// Функция корректирует задачу
+function updateTask(title) {
+    const promise = axios.put('https://repetitora.net/api/JS/Tasks', {
+        widgetId: 1336,
+        title: title
+    })
+    return promise.then((response) => {
+        return response.data;
+    })
+}
+
+// The function deletes the task by id
+function deleteTask(id) {
+    const promise = axios.delete(`https://repetitora.net/api/JS/Tasks?widgetId=1336&taskId=${id}`)
     return promise.then((response) => {
         return response.data;
     })
